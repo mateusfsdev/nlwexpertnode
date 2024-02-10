b94039cfc9c04b290378db422e5d2b6a10f6926d
mateus sant'ana <mateusfdev@outlook.com>
2024-02-10 19:54:02 -0300

    reset page routes hehehe

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,11 +2,13 @@ import fastify from "fastify";
 
 import { cretaePoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-polls";
 
 const app = fastify()
 
 app.register(cretaePoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port:4848 }).then(() => {
 })
