a81f76b8f640a9cc3c5c97f1db830ad802643ede
mateus sant'ana <mateusfdev@outlook.com>
2024-02-10 19:17:03 -0300

    create route pollcreate options 🥇✨🍚

--- src/http/routes/create-poll.ts
+++ src/http/routes/create-poll.ts
@@ -6,16 +6,26 @@ export async function cretaePoll(app: FastifyInstance) {
   app.post('/polls', async (request, reply) => {
 
     const createPollBody = z.object({
-        title: z.string()
+        title: z.string(),
+        options: z.array(z.string())
     }) 
   
-    const { title } = createPollBody.parse(request.body)
+    const { title, options } = createPollBody.parse(request.body)
   
     const poll = await prisma.poll.create({
       data: {
         title,
+        options: {
+          createMany: {
+            data: options.map(option => {
+              return { title: option}
+            })
+          }
+        }
       }
     })
+
+
   
     return reply.status(201).send({ pollId : poll.id })
   })
