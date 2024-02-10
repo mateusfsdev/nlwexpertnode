7c861055d6a38fada03e95badf1981eed2afb0e3
mateus sant'ana <mateusfdev@outlook.com>
2024-02-10 19:24:30 -0300

    register new route get in server 💾💾💾

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,12 @@
 import fastify from "fastify";
 
 import { cretaePoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
 
 const app = fastify()
 
 app.register(cretaePoll)
+app.register(getPoll)
 
 app.listen({ port:4848 }).then(() => {
 })
