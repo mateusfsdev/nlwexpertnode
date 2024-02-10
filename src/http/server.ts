006886ac7534c00dab56a914b41bb58f97ee1077
mateus sant'ana <mateusfdev@outlook.com>
2024-02-10 18:10:01 -0300

    create request to polls 🌻🍁🌻

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,8 +2,9 @@ import fastify from "fastify";
 
 const app = fastify()
 
-app.get('/hello', () => {
-    return 'hello'
+app.post('/polls', (request) => {
+    console.log(request.body.title)
+    return 'hello nlw '
 })
 
 app.listen({ port:4848 }).then(() => {
